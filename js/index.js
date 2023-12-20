6341865ea69bb13b21a8a216f46b3c63f3b1dd5f
Christof Röhl <userpattern@gmail.com>
2023-12-20 12:37:55 +0100

    task1

--- js/index.js
+++ js/index.js
@@ -9,15 +9,35 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  console.log(pizzaSize1);
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  const pizzaSize1 = pizzaInput1.value;
+  const pizzaSize2 = pizzaInput2.value;
+  console.log(pizzaSize2);
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
+// let pizzaSize1 = pizzaInput1.value;
+// console.log("Pizzasize1: " + pizzaSize1);
+// let pizzaSize2 = pizzaInput2.value;
+// console.log("Pizzasize2: " + pizzaSize2);
 
 // Task 1
 // define the function calculatePizzaGain here
+function calculatePizzaGain(pizzaSize1, pizzaSize2) {
+  let area1 = Math.PI * (pizzaSize1 / 2) ** 2;
+  let area2 = Math.PI * (pizzaSize2 / 2) ** 2;
+  let pizzaGain = ((area2 - area1) / area1) * 100;
+  console.log(area1);
+  console.log(area2);
+  console.log(area2 - area1);
+  console.log(Math.round(pizzaGain) + " %");
+  output.textContent = Math.round(pizzaGain);
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
